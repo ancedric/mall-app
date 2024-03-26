ba0e74e487a9e8f00d83527663323ce5ffa895a7
ancedric <101697896+ancedric@users.noreply.github.com>
2024-03-26 13:32:06 +0100

    Update auth0-config.js

--- frontend/auth0-config.js
+++ frontend/auth0-config.js
@@ -1,2 +1,2 @@
-export const domain = 's-mall-app.us.auth0.com'
-export const clientId = '0zsgzKkqnMZo4xPnWit5kzoYd9HerrqH'
+export const domain = 'dev-oz81ms6l3qozfgue.us.auth0.com'
+export const clientId = 'mlvbAojb6A0DmfjvhmmlRIfRaU8cK47I'
