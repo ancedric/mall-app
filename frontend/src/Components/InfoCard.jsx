dbcedd9e673979e60f55a62b48569d7a3a2d3a2a
ancedric <101697896+ancedric@users.noreply.github.com>
2024-05-07 16:36:48 +0200

    new landingpage css

--- frontend/src/Components/InfoCard.jsx
+++ frontend/src/Components/InfoCard.jsx
@@ -3,17 +3,6 @@ import PropTypes from 'prop-types'
 const InfoCard = ({infos}) => {
 
   const infoCardStyle = {
-    "width" : "24rem",
-    "box-shadow": "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
-    "margin" : "4rem",
-    "margin-bottom" : "4rem",
-    "margin-left" : "5rem",
-    "margin-right" : "5rem",
-    "border" : "2px solid",
-    "border-image" : "linear-gradient(to bottom, #fff, #2c0f4d)",
-    "border-image-slice" : "1",
-    "padding" : "10px",
-    "border-radius" : "13px",
   }
 
   const figure ={
